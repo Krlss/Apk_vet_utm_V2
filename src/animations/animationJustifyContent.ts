0541e82d3772aa44fe798147c8dc2c597b68803c
Carlos Pico <cpico6375@gmail.com>
2022-06-27 04:30:00 -0500

    Ahora si tiene coordinación el input con el logo - Reanimated

--- src/animations/animationJustifyContent.ts
+++ src/animations/animationJustifyContent.ts
@@ -1,6 +1,7 @@
 import React from "react";
 import { useSharedValue, useAnimatedStyle, withTiming } from "react-native-reanimated";
 import { height, width } from '@src/constants/animations';
+import ConfigContext from '@src/contexts/config/ConfigContext'
 
 const animationJustifyContent = () => {
 
